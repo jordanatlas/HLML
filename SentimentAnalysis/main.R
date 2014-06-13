469eedc5e028c7369cb9c77b696a5d56576327e2
jordanatlas <jordan.atlas@gmail.com>
2014-06-12 18:13:46 -0700

    Import from miluk

---
+++ SentimentAnalysis/main.R
@@ -0,0 +1,63 @@
+# packages
+library("plyr") # compute counts in aggregates
+
+# inputs
+PathTestData = "test.tsv";
+PathTrainingData = "train.tsv";
+
+#####################
+# Load data
+#####################
+
+loadData <- function(testDataPath, trainingDataPath)
+{
+  # parse test set
+  test <- read.table(PathTestData,
+                     header = TRUE,
+                     sep = "\t",
+                     comment.char = "",
+                     quote = "",
+                     colClasses = c("integer","integer","character"))
+  
+  # parse training set
+  train <- read.table(PathTrainingData,
+                      header = TRUE,
+                      sep = "\t",
+                      comment.char = "",
+                      quote = "",
+                      colClasses = c("integer","integer","character","integer"))
+  
+  
+  # combine test and train set into data
+  Sentiment = rep(as.integer(NA), nrow(test))
+  test <- cbind(test, Sentiment)
+  data <- rbind(train, test)
+  Predicted_Sentiment <- rep(as.integer(NA), nrow(data))
+  data <- cbind(data, Predicted_Sentiment)
+}
+
+if (!exists("dataset"))
+{
+  dataset = loadData(PathTestData, PathTrainingData)
+}
+
+#####################
+# Calculate features
+#####################
+
+# Determine number of characters in the phrase
+NumCharactersInPhrase = unlist(lapply(dataset$Phrase, nchar))
+
+dataset = cbind(dataset, NumCharactersInPhrase)
+
+#####################
+# Predict sentiment
+#####################
+
+dataset$Predicted_Sentiment = 2; # always predict neutral
+
+#####################
+# Validate
+#####################
+
+accuracy = round(mean(dataset$Sentiment == dataset$Predicted_Sentiment, na.rm = TRUE) * 100,2)
